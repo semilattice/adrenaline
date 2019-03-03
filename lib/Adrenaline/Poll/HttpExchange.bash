1e7d6b1fe63e45c9e6c3e284c9e53a178be26f35
Foldr <rightfold+kloe@gmail.com>
2019-03-03 22:43:58 +0100

    adrenalinePoll: implement httpExchange

--- lib/Adrenaline/Poll/HttpExchange.bash
+++ lib/Adrenaline/Poll/HttpExchange.bash
@@ -1,3 +1,23 @@
 httpExchange() {
-    echo "$@"
+    if (( $# != 3 )); then
+        1>&2 httpExchangeUsage
+        return 1
+    fi
+
+    method=$1
+    url=$2
+    timeout=$3
+
+    # The curl program accepts only positive timeouts; it treats zero timeouts
+    # as no timeout. Work around this limitation.
+    if (( $(bc <<< "$timeout <= 0") )); then
+        timeout=1
+    fi
+
+    curl -X "$method" -m "$timeout" -- "$url"
+    return "$?"
+}
+
+httpExchangeUsage() {
+    echo "Usage: $0 httpEchoExchange METHOD URL TIMEOUT"
 }
