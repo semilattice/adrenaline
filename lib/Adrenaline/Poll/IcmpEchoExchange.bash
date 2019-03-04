965a5be9c37e89cdb20ee88647b01d534ce149b7
Foldr <rightfold+kloe@gmail.com>
2019-03-04 21:10:31 +0100

    icmpEchoExchange: extract ping time

--- lib/Adrenaline/Poll/IcmpEchoExchange.bash
+++ lib/Adrenaline/Poll/IcmpEchoExchange.bash
@@ -14,10 +14,21 @@ icmpEchoExchange() {
         timeout=1
     fi
 
-    ping -c 1 -W "$timeout" -- "$address" | grep -Po '(?<=time=)[0-9.]+(?= ms)'
-    return "$?"
+    ping -c 1 -W "$timeout" -- "$address"                                   \
+        | icmpEchoExchangeExtractTime
+    return "${PIPESTATUS[0]}"
 }
 
 icmpEchoExchangeUsage() {
     echo "Usage: $0 icmpEchoExchange ADDRESS TIMEOUT"
+}
+
+icmpEchoExchangeExtractTime() {
+    # Extract the ping time from the output of the ping command, and convert it
+    # into a number of seconds.
+
+    local dt=$(grep -Po -m 1 '(?<=time=)[0-9.]+(?= ms)')
+    if [[ $dt != '' ]]; then
+        bc <<< "scale = 4; $dt / 1000.0"
+    fi
 }
