776614166e347d4b9a3e507638cbc2e960d1b7ee
Foldr <rightfold+kloe@gmail.com>
2019-03-04 20:06:55 +0100

    Documentation: document build and environment

--- adrenaline.nix
+++ adrenaline.nix
@@ -12,12 +12,16 @@ stdenv.mkDerivation {
     installPhase = ''
         mkdir -p "$out/bin" "$out/share"
 
-        makeWrapper '${bash}/bin/bash' "$out/bin/adrenalinePoll"            \
-            --prefix 'PATH' ':' '${bc}/bin'                                 \
-            --prefix 'PATH' ':' '${curl}/bin'                               \
-            --set 'ADRENALINEBIN' "$out/share/bin"                          \
-            --set 'ADRENALINELIB' "$out/share/lib"                          \
-            --add-flags "$out/share/bin/adrenalinePoll.bash"
+        bashProgram() {
+            makeWrapper '${bash}/bin/bash' "$out/bin/$1"                    \
+                --prefix 'PATH' ':' '${bc}/bin'                             \
+                --prefix 'PATH' ':' '${curl}/bin'                           \
+                --set 'ADRENALINEBIN' "$out/share/bin"                      \
+                --set 'ADRENALINELIB' "$out/share/lib"                      \
+                --add-flags "$out/share/bin/$1.bash"
+        }
+
+        bashProgram 'adrenalinePoll'
 
         cp -R 'bin' "$out/share"
         cp -R 'lib' "$out/share"
