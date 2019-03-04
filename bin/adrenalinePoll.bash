0d9e6b864aa5dd4c51e7ed5b4fba80a92ef96e33
Foldr <rightfold+kloe@gmail.com>
2019-03-04 21:12:40 +0100

    adrenalinePoll: move usage to top of file

--- bin/adrenalinePoll.bash
+++ bin/adrenalinePoll.bash
@@ -3,16 +3,16 @@ set -o nounset
 source "$ADRENALINELIB/Adrenaline/Poll/HttpExchange.bash"
 source "$ADRENALINELIB/Adrenaline/Poll/IcmpEchoExchange.bash"
 
+mainUsage() {
+    echo "Usage: $0 POLLTYPE [ARGUMENT ...]"
+}
+
 main() {
     case ${1-} in
         (icmpEchoExchange) icmpEchoExchange "${@:2}" ; return "$?" ;;
         (httpExchange)     httpExchange     "${@:2}" ; return "$?" ;;
         (*)                1>&2 mainUsage            ; return 1    ;;
     esac
-}
-
-mainUsage() {
-    echo "Usage: $0 POLLTYPE [ARGUMENT ...]"
 }
 
 main "$@"
